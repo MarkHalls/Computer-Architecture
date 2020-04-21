b0ba8817c38a32c095ef73e82eec084b1201ca4d
Mark Halls <mark.halls@gmail.com>
2020-04-21 14:45:06 -0700

    Load program from file

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,7 @@
 """CPU functionality."""
 
 import sys
+import re
 
 ADD = 0b10100000
 SUB = 0b10100001
@@ -49,22 +50,16 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
+        program_filename = sys.argv[1]
 
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010,  # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111,  # PRN R0
-            0b00000000,
-            0b00000001,  # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(program_filename) as f:
+            regex = re.compile("\d+")
+            for line in f:
+                matched = regex.match(line)
+                if matched:
+                    instruction = int(matched.group(), 2)
+                    self.ram[address] = instruction
+                    address += 1
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
